536816c13ce854bb5e43a0b76e9f82c6a5880729
Maaz Adil <maazadil786@gmail.com>
2022-07-30 09:05:19 -0700

    first commit

---
+++ robotController/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
+# Create your views here.
