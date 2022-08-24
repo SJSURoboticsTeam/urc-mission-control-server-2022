57ca8c0aef1c9afa888cfabf6bf12c27a0303898
Maaz Adil <maazadil786@gmail.com>
2022-08-24 11:33:07 -0700

    sample code for post and get commadns

--- robotServer/views.py
+++ robotServer/views.py
@@ -5,10 +5,36 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-
 from django.shortcuts import render
+
+import socket
+import sys
+import json
+
+# Create your views here.
 
 def index(request):
     return render(request, "robotServer/index.html")
 
-# Create your views here.
+def toRover(request):
+    HOST, PORT = "localhost", 3000
+
+    m ='{"hearbeat": 2}'
+    jsonObj = json.loads(m)
+
+    data = jsonObj
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+        sock.sendall(jsonObj)
+
+        # Receive data from the server and shut down
+        received = sock.recv(5000)
+    finally:
+        sock.close()
+
+    print "Sent:     {}".format(data)
+    print "Received: {}".format(received)
